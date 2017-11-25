0765dfbf61bf2cf17fe81cf99e1f6f5c29ae41c6
Angel Newton <xanewton@gmail.com>
2017-11-25 15:02:20 -0500

    Add single view proyect

---
+++ MyMovieManager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyMovieManager
+//
+//  Created by xengar on 2017-11-25.
+//  Copyright © 2017 xengar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
